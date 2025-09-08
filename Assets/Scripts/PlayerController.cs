f491865fe35ce3c2727ae2e75f74b6c36ad1b646
Jogo1001 <101247726@georgebrown.ca>
2025-09-08 17:51:58 -0400

    playercontroller script added, walking animations added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,89 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float movespeed;
-    public Vector2 input;
+    public float PlayerMoveSpeed = 5f;
 
-    public Rigidbody2D PlayerRb;
+
     public bool IsPlayerMoving;
 
-    void Start()
+    private Vector2 Movement;
+    public Rigidbody2D PlayerRB;
+
+    private Animator GeorgeAnimator;
+
+    private void Awake()
     {
-        
+        GeorgeAnimator = GetComponent<Animator>();
     }
 
-  
+
+
+
     void Update()
     {
-       if(!IsPlayerMoving)
+
+        Movement.x = Input.GetAxisRaw("Horizontal");
+        Movement.y = Input.GetAxisRaw("Vertical");
+
+
+        // remove diagonal movement
+        if (Movement.y != 0)
+        {
+            Movement.x = 0;
+        }
+
+
+        // George Walk Animation
+        if (Movement != Vector2.zero)
         {
 
+            GeorgeAnimator.SetFloat("MoveX", Movement.x);
+            GeorgeAnimator.SetFloat("MoveY", Movement.y);
+            IsPlayerMoving = true;
+
+            ResetIdle();
         }
+        else
+        {
+            // George stopped moving
+            IsPlayerMoving = false;
+
+            // set idle direction depending on last movement
+            if (GeorgeAnimator.GetFloat("MoveX") < 0)
+            {
+                GeorgeAnimator.SetBool("LeftIdle", true);
+            }
+            else if (GeorgeAnimator.GetFloat("MoveX") > 0)
+            {
+                GeorgeAnimator.SetBool("RightIdle", true);
+            }
+            else if (GeorgeAnimator.GetFloat("MoveY") > 0)
+            {
+                GeorgeAnimator.SetBool("UpIdle", true);
+            }
+            else if (GeorgeAnimator.GetFloat("MoveY") < 0)
+            {
+                GeorgeAnimator.SetBool("DownIdle", true);
+            }
+        }
+
+    }
+    void FixedUpdate()
+    {
+        // George Movement
+        if (Movement != Vector2.zero)
+        {
+            PlayerRB.MovePosition(PlayerRB.position + Movement * PlayerMoveSpeed * Time.fixedDeltaTime);
+
+        }
+
+    }
+    public void ResetIdle()
+    {
+
+        GeorgeAnimator.SetBool("LeftIdle", false);
+        GeorgeAnimator.SetBool("RightIdle", false);
+        GeorgeAnimator.SetBool("UpIdle", false);
+        GeorgeAnimator.SetBool("DownIdle", false);
     }
 }
